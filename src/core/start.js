17548161f7fd23bbebc61f53496a9a23b76d6358
Maizan <memaizanx@gmail.com>
2026-02-01 04:29:16 +0300

    v3.0.37: Revert to devtunnel-cli.vercel.app, sync versions

--- src/core/start.js
+++ src/core/start.js
@@ -272,7 +272,7 @@ async function main() {
   console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
   console.log("Repository: https://github.com/maiz-an/DevTunnel-CLI");
   console.log("npm Package: https://www.npmjs.com/package/devtunnel-cli");
-  console.log("Website: https://maizan.me/devtunnel-cli");
+  console.log("Website: https://devtunnel-cli.vercel.app");
   console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
   console.log("");
 
